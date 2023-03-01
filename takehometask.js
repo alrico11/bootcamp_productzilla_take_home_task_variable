dfb59906ab2110ba35c3d9ecc3d7effee7e09472
Alrico Rizki <alricosaiyan123@gmail.com>
2023-03-01 20:30:50 +0700

    TUGAS Take Home Task - Variable

---
+++ takehometask.js
@@ -0,0 +1,145 @@
+// var list_var = [];
+// var list_angka1 = [];
+// var list_angka2 = [];
+// var list_hasil = [];
+// for (var i = 0; i < 20; i++) {
+//   list_var[i] = 'angka' + i;
+//   list_angka1[i] = +i;
+//   list_angka2[i] = i + i
+// }
+
+// for (var i = 0; i < 20; i++) {
+//   list_hasil[i] = list_angka1[i] + list_angka2[i] + list_angka2[3+i] +
+//   console.log("Penjumlahan " + list_angka1[i] + " dengan " + list_angka2[i] + " hasilnya " + list_hasil[i])
+// }
+
+var angka = 1 + 1 + 1 + 1 + 1,
+  angka1 = 2 - 2 - 2 - 2 - 2,
+  angka2 = 3 * 3 * 3 * 3 * 3,
+  angka3 = 4 / 4 / 4 / 4 / 4,
+  angka4 = 5 ** 2 / 2 + 4 + 4 + 2,
+  angka5 = 4 - 3 + 3 * 2 / 2,
+  angka6 = 3 / 3 * 5 + 5 + 2,
+  angka7 = 4 / 8 * 2 - 6 + 2,
+  angka8 = 2 / 6 * 3 * 5 ** 2,
+  angka9 = 4 + 6 + 5 / 3 * 4,
+  angka10 = 5 / 6 * 4 + 5 - 3,
+  angka11 = 5 / 5 - 6 * 4 + 2,
+  angka12 = 6 * 5 / 4 + 2 - 5,
+  angka13 = 6 / 5 * 5 + 2 + 4,
+  angka14 = 5 / 5 * 4 * 4 - 8,
+  angka15 = 6 * 6 * -5 / 5 + 1,
+  angka16 = 9 / 3 * 4 - 7 + 9,
+  angka17 = 8 / 4 * 1 ** 2 + 5,
+  angka18 = 5 / 5 * 6 / 2 * 5,
+  angka19 = 7 * 8 / 2 * 4 - 5
+  console.log(angka)
+  console.log(angka1)
+  console.log(angka2)
+  console.log(angka3)
+  console.log(angka4)
+  console.log(angka5)
+  console.log(angka6)
+  console.log(angka7)
+  console.log(angka8)
+  console.log(angka9)
+  console.log(angka10)
+  console.log(angka11)
+  console.log(angka12)
+  console.log(angka13)
+  console.log(angka14)
+  console.log(angka15)
+  console.log(angka16)
+  console.log(angka17)
+  console.log(angka18)
+  console.log(angka19)
+
+
+let
+perbandinganlebihdari1 = 1 > 2,
+perbandinganlebihdari2 = 3 > 2,
+perbandinganlebihdari3 = 5 > 4,
+perbandinganlebihdari4 = 0 > 5,
+perbandinganlebihdari5 = 5 > 5
+console.log(perbandinganlebihdari1)
+console.log(perbandinganlebihdari2)
+console.log(perbandinganlebihdari3)
+console.log(perbandinganlebihdari4)
+console.log(perbandinganlebihdari5)
+
+perbandingankurangdari1 = 1 < 1,
+perbandingankurangdari2 = 1 < 2,
+perbandingankurangdari3 = 5 < 2,
+perbandingankurangdari4 = 8 < 9,
+perbandingankurangdari5 = 5 < 4,
+console.log(perbandingankurangdari1)
+console.log(perbandingankurangdari2)
+console.log(perbandingankurangdari3)
+console.log(perbandingankurangdari4)
+console.log(perbandingankurangdari5)
+
+perbandinganlebihdarisamadengan1 = 1 >= 2,
+perbandinganlebihdarisamadengan2 = 2 >= 2,
+perbandinganlebihdarisamadengan3 = 0 >= 2,
+perbandinganlebihdarisamadengan4 = 3 >= 2,
+perbandinganlebihdarisamadengan5 = 7 >= 5,
+console.log(perbandinganlebihdarisamadengan1)
+console.log(perbandinganlebihdarisamadengan2)
+console.log(perbandinganlebihdarisamadengan3)
+console.log(perbandinganlebihdarisamadengan4)
+console.log(perbandinganlebihdarisamadengan5)
+
+perbandingankurangdarisamadengan1 = 2 <= 2,
+perbandingankurangdarisamadengan2 = 1 <= 2,
+perbandingankurangdarisamadengan3 = 5 <= 8,
+perbandingankurangdarisamadengan4 = 2 <= 4,
+perbandingankurangdarisamadengan5 = 8 <= 2,
+console.log(perbandingankurangdarisamadengan1)
+console.log(perbandingankurangdarisamadengan2)
+console.log(perbandingankurangdarisamadengan3)
+console.log(perbandingankurangdarisamadengan4)
+console.log(perbandingankurangdarisamadengan5)
+
+perbandingansamadengan1 = 8 == 2,
+perbandingansamadengan2 = 2 == 2,
+perbandingansamadengan3 = 1 == 2,
+perbandingansamadengan4 = 5 == 2,
+perbandingansamadengan5 = 8 == 8,
+console.log(perbandingansamadengan1)
+console.log(perbandingansamadengan2)
+console.log(perbandingansamadengan3)
+console.log(perbandingansamadengan4)
+console.log(perbandingansamadengan5)
+
+perbandinganperbandingantypedata1 = '2' === '2',
+perbandinganperbandingantypedata2 = '2' === 2,
+perbandinganperbandingantypedata3 = 2 === 2,
+perbandinganperbandingantypedata4 = 5 === '10',
+perbandinganperbandingantypedata5 = '5' === '2',
+console.log(perbandinganperbandingantypedata1)
+console.log(perbandinganperbandingantypedata2)
+console.log(perbandinganperbandingantypedata3)
+console.log(perbandinganperbandingantypedata4)
+console.log(perbandinganperbandingantypedata5)
+
+perbandingantidaksamadengan1 = 5 != 2,
+perbandingantidaksamadengan2 = 2 != 2,
+perbandingantidaksamadengan3 = 6 != 2,
+perbandingantidaksamadengan4 = 5 != 5,
+perbandingantidaksamadengan5 = 3 != 2,
+console.log(perbandingantidaksamadengan1)
+console.log(perbandingantidaksamadengan2)
+console.log(perbandingantidaksamadengan3)
+console.log(perbandingantidaksamadengan4)
+console.log(perbandingantidaksamadengan5)
+
+perbandingantidaksamadengantypedata1 = 2 !== "2",
+perbandingantidaksamadengantypedata2 = 2 !== 2,
+perbandingantidaksamadengantypedata3 = "2" !== "2",
+perbandingantidaksamadengantypedata4 = 2 !== "8",
+perbandingantidaksamadengantypedata5 = "2" !== 2
+console.log(perbandingantidaksamadengan1)
+console.log(perbandingantidaksamadengan2)
+console.log(perbandingantidaksamadengan3)
+console.log(perbandingantidaksamadengan4)
+console.log(perbandingantidaksamadengan5)
